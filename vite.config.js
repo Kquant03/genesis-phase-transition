b61471ee70ff80743a9fd430df0c3c4913eaf179
kquant <kquant03@gmail.com>
2026-04-06 13:18:34 -0400

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/genesis-phase-transitions/',
+  base: '/genesis-phase-transition/',
   server: {
     port: 3000,
     open: true
